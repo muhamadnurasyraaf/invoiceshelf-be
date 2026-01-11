8848b8443f68f7f28dbd1525f7b9e23eb7c1e908
muhamadnurasyraaf <masyraaf14@gmail.com>
2026-01-11 20:44:20 +0800

    added origins

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: ['https://invoiceshelf-fe.vercel.app', 'http://localhost:3000'],
     credentials: true,
   });
 
